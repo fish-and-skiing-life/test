b5dd86c695504071f8b08f67735276333655c21d
伊藤里香 <itourika@itourika-no-MacBook-Pro.local>
2018-06-04 15:29:20 +0900

    【6/4の報告】

--- test-selenium.rb
+++ test-selenium.rb
@@ -3,20 +3,24 @@ require 'roo'
 
 xlsx = Roo::Excelx.new('./SPRAY for Agencyページ一覧.xlsx')
 xlsx.default_sheet = 'Webページ'
-
-2.upto(xlsx.last_row) do |row|  
-  url =  xlsx.cell(row, 2)    #2列目を1行ずつ取得する
-  url = url.sub(/\(.:format\)/ , "")
-  break
-end 
-url = "http://agency.sp-ray.com/"
-url += xlsx.cell(15,2) 
-url = url.sub(/\(.:format\)/ , "")
-p url
-
 driver = Selenium::WebDriver.for :chrome
-driver.get url
+mail_ad = "tettyann0405@gmail.com"
+pass = "arigatou"
+sign_in = "http://agency.sp-ray.com/agencies/sign_in"
+2.upto(xlsx.last_row) do |row|  
+  if xlsx.cell(row, 4) == "Webページ" then
+     driver.get sign_in
+     driver.find_element(:id, 'agency_email').send_keys mail_ad
+     driver.find_element(:id, 'agency_password').send_keys pass
+     driver.find_element(:tag_name, "button").click
+     url = "http://agency.sp-ray.com/"
+     url +=  xlsx.cell(row, 2)    #2列目を1行ずつ取得する
+     url = url.sub(/\(.:format\)/ , "")
+     driver.get url
+     wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+     sleep 2
+     driver.find_element(:link_text => 'ログアウト').click
+  end
+end
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-sleep 10
 driver.quit
